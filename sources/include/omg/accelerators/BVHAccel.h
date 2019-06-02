4e4eea3d68763483aeef7bf0a2489577a098507d
Vitor Greati <greati@ufrn.edu.br>
2019-06-02 01:15:47 -0300

    Implement basic BVH infrastructure

--- sources/include/omg/accelerators/BVHAccel.h
+++ sources/include/omg/accelerators/BVHAccel.h
@@ -18,6 +18,73 @@ class BVHAccel : public AggregatePrimitive {
 
     public:
 
+        /**
+         * Types of partition strategy.
+         * */
+        enum class SplitMethod {
+            SAH,
+            HLBVH,
+            Middle,
+            EqualCounts
+        };
+
+    private:
+
+        const int _max_prims_node;
+        const SplitMethod _split_method;
+        std::vector<std::shared_ptr<Primitive>> _primitives;
+
+        /**
+         * Information of each primitive in the tree.
+         * */
+        struct BVHPrimitiveInfo {
+            size_t prim_number;
+            Bounds3 bounds;
+            Point3 centroid;
+
+            BVHPrimitiveInfo() {/*empty*/}
+
+            BVHPrimitiveInfo(size_t prim_number, const Bounds3& bounds)
+            : prim_number {prim_number}, bounds {bounds},
+                centroid {.5f * bounds.pMin + .5f * bounds.pMax}
+            {/*empty*/}
+        };
+
+        /**
+         * O node in the BVH tree.
+         * */
+        struct BVHBuildNode {
+            Bounds3 bounds;
+            BVHBuildNode *children[2];
+            int split_axis, first_prim_offset, n_primitives;
+
+            void init_leaf(int first, int n, const Bounds3& b) {
+                first_prim_offset = first;
+                n_primitives = n;
+                bounds = b;
+                children[0] = children[1] = nullptr;
+            }
+
+            void init_interior(int axis, BVHBuildNode* c0, BVHBuildNode* c1) {
+                children[0] = c0;
+                children[1] = c1;
+                bounds = c0->bounds.get_union(c0->bounds, c1->bounds);
+                split_axis = axis;
+                n_primitives = 0;
+            }
+
+
+        };
+
+    public:
+
+        BVHAccel(const std::vector<std::shared_ptr<Primitive>>& primitives,
+                int max_prims_node, SplitMethod split_method);
+
+        BVHBuildNode* recursive_build(std::vector<BVHPrimitiveInfo>& prim_info, 
+                int start, int end, int* total_nodes,
+                std::vector<std::shared_ptr<Primitive>>& ordered_prims);
+
         virtual bool intersect(const Ray& ray, SurfaceInteraction* interaction) const;
 
         virtual bool intersect(const Ray& ray) const;
--- sources/src/accelerators/BVHAccel.cpp
+++ sources/src/accelerators/BVHAccel.cpp
@@ -2,6 +2,38 @@
 
 using namespace omg;
 
+BVHAccel::BVHAccel(const std::vector<std::shared_ptr<Primitive>>& primitives,
+                int max_prims_node, SplitMethod split_method)
+    : _primitives {primitives}, 
+      _max_prims_node {std::max(255, max_prims_node)}, 
+      _split_method {split_method}{
+
+    if (primitives.size() == 0)
+        return;
+
+    //> Compute the tree
+    //- Initialize vector of infos
+    std::vector<BVHPrimitiveInfo> prim_infos {_primitives.size()};
+    for (size_t i {0}; i < prim_infos.size(); ++i) {
+        prim_infos[i] = BVHPrimitiveInfo{i, _primitives[i]->world_bound()};
+    }
+    //- Call the appropriate method
+    int total_nodes = 0;
+    std::vector<std::shared_ptr<Primitive>> ordered_prims;
+    BVHBuildNode* root;
+    if (split_method == SplitMethod::HLBVH)
+        throw std::invalid_argument("unavailable option");
+    else
+        root = recursive_build(prim_infos, 0, _primitives.size(), &total_nodes, ordered_prims);
+    _primitives.swap(ordered_prims);
+}
+
+
+BVHAccel::BVHBuildNode* BVHAccel::recursive_build(std::vector<BVHPrimitiveInfo>& prim_info, 
+        int start, int end, int* total_nodes,
+        std::vector<std::shared_ptr<Primitive>>& ordered_prims) {
+
+}
 
 bool BVHAccel::intersect(const Ray& ray, SurfaceInteraction* interaction) const {
     return false;
