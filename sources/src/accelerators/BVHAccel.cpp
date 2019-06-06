22ab4ee44e2aebc464a31c1d793a0d7aeec2fd84
Vitor Greati <greati@ufrn.edu.br>
2019-06-05 23:16:38 -0300

    Fix one part of BVH

--- sources/src/accelerators/BVHAccel.cpp
+++ sources/src/accelerators/BVHAccel.cpp
@@ -33,6 +33,70 @@ BVHAccel::BVHBuildNode* BVHAccel::recursive_build(std::vector<BVHPrimitiveInfo>&
         int start, int end, int* total_nodes,
         std::vector<std::shared_ptr<Primitive>>& ordered_prims) {
 
+    std::unique_ptr<BVHBuildNode> node = std::make_unique<BVHBuildNode>();
+    (*total_nodes)++;
+
+    Bounds3 bounds;
+    for (auto i {start}; i < end; ++i)
+        bounds = bounds.get_union(bounds, prim_info[i].bounds);
+
+    int n_primitives = end - start;
+    if (n_primitives == 1) {
+        int first_prim_offset = ordered_prims.size();
+        for (auto i {start}; i < end; ++i) {
+            int prim_num = prim_info[i].prim_number;
+            ordered_prims.push_back(_primitives[prim_num]);
+        }
+        node->init_leaf(first_prim_offset, n_primitives, bounds);
+        return node.get();
+    } else {
+
+        Bounds3 centroid_bounds;
+        for (auto i {start}; i < end; ++i) {
+            centroid_bounds = centroid_bounds.get_union(centroid_bounds, prim_info[i].centroid);
+        }
+        int dim = centroid_bounds.maximum_extent();
+
+        int mid = (start + end) / 2;
+        if (centroid_bounds.pMax(dim) == centroid_bounds.pMin(dim)) {
+            // leaf node
+            int first_prim_offset = ordered_prims.size();
+            for (auto i {start}; i < end; ++i) {
+                int prim_num = prim_info[i].prim_number;
+                ordered_prims.push_back(_primitives[prim_num]);
+            }
+            node->init_leaf(first_prim_offset, n_primitives, bounds);
+            return node.get();
+        } else {
+            switch(_split_method) {
+                case SplitMethod::Middle: {
+                    float pmid = (centroid_bounds.pMin(dim) + centroid_bounds.pMax(dim)) / 2;
+                    BVHPrimitiveInfo* midPtr =
+                        std::partition(&prim_info[start], &prim_info[end - 1] + 1,
+                                [dim, pmid](const auto& pi) {
+                                    return pi.centroid(dim) < pmid; 
+                                });
+                    mid = midPtr - &prim_info[0];
+                    if (mid != start && mid != end)
+                        break;
+                }
+                case SplitMethod::EqualCounts:
+                    mid = (start + end) / 2;
+                    std::nth_element(&prim_info[start], &prim_info[mid], &prim_info[end-1]+1,
+                            [dim](const BVHPrimitiveInfo& a, const BVHPrimitiveInfo& b) {
+                                return a.centroid(dim) < b.centroid(dim);
+                            });
+                    break;
+            }
+            // partition
+            node->init_interior(dim,
+                    recursive_build(prim_info, start, mid, total_nodes, ordered_prims),
+                    recursive_build(prim_info, mid, end, total_nodes, ordered_prims));
+        }
+
+    }
+    
+    return node.get();
 }
 
 bool BVHAccel::intersect(const Ray& ray, SurfaceInteraction* interaction) const {
