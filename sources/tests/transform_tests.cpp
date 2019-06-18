3ef5e15aff37f41d8ca86459ef76fdac98a42b31
Vitor Greati <greati@ufrn.edu.br>
2019-06-18 20:19:19 -0300

    Start transform tests

---
+++ sources/tests/transform_tests.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+//#include "omg/raytracer/TransformCache.h"
+
+namespace {
+
+    TEST(TransformCache, Lookup) {
+    
+        //omg::TransformCache tc;
+
+    }
+
+
+};
